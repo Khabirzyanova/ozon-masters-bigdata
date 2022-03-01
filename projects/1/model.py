47d992f58c8e064e1c1522f0d4e0e9490b45f8f5
Aliya <31696065+Khabirzyanova@users.noreply.github.com>
2022-03-01 16:10:26 +0300

    Update model.py

--- projects/1/model.py
+++ projects/1/model.py
@@ -1,15 +1,41 @@
+%matplotlib inline
+%config InlineBackend.figure_format = 'svg'
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import gc, itertools, time, math
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import category_encoders as ce
 from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, log_loss, roc_auc_score
+from sklearn.preprocessing import OneHotEncoder, KBinsDiscretizer, LabelEncoder, RobustScaler
+from catboost import CatBoostClassifier
+from xgboost import XGBClassifier
+from lightgbm import plot_metric
+from sklearn.model_selection import train_test_split
+
+# from sklearn.compose import ColumnTransformer
+# from sklearn.pipeline import Pipeline
+# from sklearn.impute import SimpleImputer
+# from sklearn.preprocessing import StandardScaler, OneHotEncoder
+# from sklearn.linear_model import LinearRegression
+# from sklearn.model_selection import train_test_split, GridSearchCV
 
 #
 # Dataset fields
 #
-fields = """doc_id,hotel_name,hotel_url,street,city,state,country,zip,class,price,
-num_reviews,CLEANLINESS,ROOM,SERVICE,LOCATION,VALUE,COMFORT,overall_ratingsource""".replace("\n",'').split(",")
+# fields = """doc_id,hotel_name,hotel_url,street,city,state,country,zip,class,price,
+# num_reviews,CLEANLINESS,ROOM,SERVICE,LOCATION,VALUE,COMFORT,overall_ratingsource""".replace("\n",'').split(",")
+numeric_features = ["if"+str(i) for i in range(1,14)]
+categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
+
+fields = ["id", "label"] + numeric_features + categorical_features
 
 #
 # Model pipeline
