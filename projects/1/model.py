a0bc7e4acb363fb2b908ed2479e55cd36effa270
Aliya Khabirzyanova <31696065+Khabirzyanova@users.noreply.github.com>
2022-03-04 15:50:07 +0300

    Update model.py

--- projects/1/model.py
+++ projects/1/model.py
@@ -1,5 +1,5 @@
 from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline 
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.linear_model import LinearRegression
@@ -17,15 +17,17 @@ fields = ["id", "label"] + numeric_features + categorical_features
 # Model pipeline
 # We create the preprocessing pipelines for both numeric and categorical data.
 numeric_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='median')),
-#    ('scaler', StandardScaler())
+    ('imputer', SimpleImputer(strategy='median')), # Imputation transformer for completing missing values.
+# If “median”, then replace missing values using the median along each column. Can only be used with numeric data.
+#    ('scaler', StandardScaler()) # Standardize features by removing the mean and scaling to unit variance.
 ])
 
 categorical_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
-    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')), # If “constant”, then replace missing values with fill_value. 
+    ('onehot', OneHotEncoder(handle_unknown='ignore')) # Encode categorical features as a one-hot numeric array.
 ])
 
+# Applies transformers to columns of an array or pandas DataFrame.
 preprocessor = ColumnTransformer(
     transformers=[
         ('num', numeric_transformer, numeric_features),
