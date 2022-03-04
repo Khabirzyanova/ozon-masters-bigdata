722f923f63a9dff449dfe9c7e23c14f1084cd2c3
Aliya Khabirzyanova <31696065+Khabirzyanova@users.noreply.github.com>
2022-03-04 17:15:40 +0300

    Update model.py

--- projects/1/model.py
+++ projects/1/model.py
@@ -5,8 +5,8 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split, GridSearchCV
 
-import tensorflow as tf
-from keras.utils.np_utils import to_categorical
+# import tensorflow as tf
+# from keras.utils.np_utils import to_categorical
 
 
 # Dataset fields
