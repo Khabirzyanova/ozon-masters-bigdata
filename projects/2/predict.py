5cff731c6ed73a59a342f38154f2e793b48d10af
Aliya Khabirzyanova <31696065+Khabirzyanova@users.noreply.github.com>
2022-03-18 00:14:30 +0300

    Update predict.py

--- projects/2/predict.py
+++ projects/2/predict.py
@@ -17,7 +17,7 @@ logging.info("SCRIPT CALLED AS {}".format(sys.argv[0]))
 logging.info("ARGS {}".format(sys.argv[1:]))
 
 #load the model
-model = load("1.joblib")
+model = load("2.joblib")
 
 #fields = """doc_id,hotel_name,hotel_url,street,city,state,country,zip,class,price,
 #num_reviews,CLEANLINESS,ROOM,SERVICE,LOCATION,VALUE,COMFORT,overall_ratingsource""".replace("\n",'').split(",")
@@ -25,7 +25,7 @@ model = load("1.joblib")
 #read and infere
 read_opts=dict(
         sep='\t', names=valid_fields, index_col=False, header=None,
-        iterator=True, chunksize=500
+        iterator=True, chunksize=500, na_values='\\N'
 )
 
 for df in pd.read_csv(sys.stdin, **read_opts):
